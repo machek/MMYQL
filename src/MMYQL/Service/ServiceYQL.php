28518ad498bd928ea5789f691a9980b181c30481
Zdenek Machek <zdenek.machek@gmail.com>
2013-04-27 16:33:25 +0100

    simple implementation

--- src/MMYQL/Service/ServiceYQL.php
+++ src/MMYQL/Service/ServiceYQL.php
@@ -8,17 +8,54 @@
 namespace MMYQL\Service;
 
 
+use Zend\Http\Client;
+
 class ServiceYQL {
+
+    /**
+     * @var \Zend\Http\Client
+     */
+    private $client;
 
 
     public function __construct($config){
 
+        if(!isset($config['yql_base_url'])) throw new \InvalidArgumentException('Missing yql_base_url in config');
+
+        $this->client = new Client();
+        $this->client->setUri($config['yql_base_url']);
+        $this->client->setOptions(array(
+            'maxredirects' => 0,
+            'timeout'      => 10
+        ));
+
     }
 
 
-    public function test(){
+    /**
+     * @param $select
+     * @param $from
+     * @param $where
+     * @param $format
+     */
+    public function executeQuery($select , $from, $where, $format = 'json'){
 
-        echo "ok";
+        $yql_query = "select {$select} from {$from} where {$where} ";
+
+        $this->client->setParameterGet(array(
+            'q'  => $yql_query,
+            'format'   => $format
+        ));
+
+        $response = $this->client->send();
+
+        /* @var $response \Zend\Http\Response */
+        if ($response->isSuccess()) {
+            return $response->getBody();
+        }
+        else{
+            throw new \Exception('Error - Response code: '.$response->getStatusCode());
+        }
     }
 
 }
